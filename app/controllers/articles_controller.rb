a707a10a22e3a0bf7fda8e47422ef39ae7078f39
JMedina <jmedina@magnastudios.net>
2016-01-19 04:58:58 +0000

    Ready to deploy and extract dedundency

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def new
     @article = Article.new
@@ -10,7 +11,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+
   end
   
   
@@ -27,7 +28,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article Updated"
       redirect_to article_path(@article)
@@ -37,11 +38,11 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+
   end
   
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "Article Deleted"
     redirect_to articles_path
@@ -50,9 +51,15 @@ class ArticlesController < ApplicationController
  
   
   private
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    
     def article_params
       params.require(:article).permit(:title, :description)
-      
     end
+    
+    
   
 end
